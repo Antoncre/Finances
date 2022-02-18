aac7196655aed4b100780342ae8a8725ea605fcb
Anton <97456996+Antoncre@users.noreply.github.com>
2022-02-18 18:17:25 +0100

    Updated usage

--- datas/lang_help.py
+++ datas/lang_help.py
@@ -1,7 +1,4 @@
-
 h_ua = """
-
-
 
 
 ФІНАНСОВА ПРОГРАМА
@@ -20,36 +17,30 @@ h_ua = """
 
 
 
-
                                                             автор: Антоній Гуйван
                 """
 h_pl = """
-        
-        
 
 
 APLIKACJA FINANSOWA
-        
-        
-        
-        
-        
-        
+
+
+
+
+
+
 Aplikacja służy do 
 zapisu przychodów lub wydatków wraz z datą
 i opisem, które zostaną dodane do listy 
 i wyświetlone w posortowany sposób według daty.
 
-                                                            
-                                                            
-                                                            
-                                                            
-                                                            
+
+
+
+
                                                             autor: Antonij Gujwan
         """
 h_en = """
-
-
 
 
 FINANCES PROGRAM
@@ -68,280 +59,255 @@ sorted and printed in nice looking way.
 
 
 
-
                                                         the author: Antonij Gujwan
                 """
 hu_ua = """
-
-
                                                      КОРИСТУВАННЯ
 
 
 1. Введення та додавання елементу до списку
-    a) Введення суми
-    b) Введення дати
-    c) Введення опису
-    d) Додавання до списку
-
+   a) Введення суми
+   б) Введення дати
+   в) Введення опису
+   г) Додавання до списку
 2. Зміна структури
 3. Зміна мови
 4. Видалення елементів
- 
-
+   а) Видалення вибраних елементів
+   б) Видалення усіх елементів
 
 1.
-    
+
     Варто пам'ятати що на кожному етапі вводу можна скористуватись кнопкою
     «Відмінити» щоб відмінити увесь процесс вводу нового елементу,
     що спричинить видалення усіх введених раніше даних.
-    
+
 a)
-    
+
     Щоб ввести суму потрібно ввести в текстовому полі 
     (зелений фон) число, можна відділити крапкою або комою. 
     Приклади: «380»; «24,23»; «15.22»; «-2»; «-3.8».
     Числа без знаку вважаються за прибуток,
     а числа з мінусом перед, вважаються за витрату.
     Після чого потрібно клацнути кнопку з символом «↲» або натиснути ентер. 
-    
+
     Увага! Кнопка не запрацює якщо вартість неправильна,
     поправте вартість та спробуйте ще раз.
-    
-b)
-    
+
+б)
+
     Щоб ввести дату, потрібно ввести в текстовому полі (зелений фон) 
     дату відділену за допомогою тере, такий формат: РРРР-ММ-ДД 
     Приклади: «2000-02-29»; «1998-12-07»; «2022-01-13».
     Після чого потрібно клацнути кнопку з символом «↲» або натиснути ентер. 
-    
+
     Увага! Кнопка не запрацює якщо вартість неправильна,
     поправте вартість та спробуйте ще раз.
-    
-c)
-    
+
+в)
+
     Щоб ввести опис, потрібно ввести в текстовому полі (зелений фон) 
     свій опис. Приклади: «Потратив на базарі»; «Заробив»; «Найшов на дорозі».
     Після чого потрібно клацнути кнопку з символом «↲» або натиснути ентер. 
-    
-d)
-    
+
+г)
+
     Щоб додати елемент до списку варто спочатку перевірити чи дані правильні.
-    
-    Увага! Функція видалення вибраних елементів ще не створена,
-    якщо не хочемо видаляти усіх елементів
-    варто подумати перед добавленням. 
-    
     Після того як ми вже впевнились що хочемо додати наш елемент до списку,
     клацаємо кнопку «Підтвердити».
-    
-2. 
 
+2. 
     Щоб змінити вигляд структури списку клацаємо символ «⧫» на панелі меню 
     в лівому верхньому кутку, після чого вибираємо «Датовано» 
     або «Структура Антона». Структура за умовчанням «Датовано».
-    
-3.
 
+3.
     Щоб змінити мову клацаємо символ «⨁» на панелі меню 
     в лівому верхньому кутку та вибираємо потрібну нам мову.
-    
+
     Увага! Мову потрібно змінити перед вводом даних,
     введення даних та зміна мови, спричинить 
     видалення введених і не записаних до списку даних.
-    
+
 4.
 
-    Шоб видалити всі елементи зі списку клацаємо символ «⨂» на панелі меню 
+а)  Шоб видалити вибрані елементи зі списку клацаємо символ «⨂» на панелі 
+    меню в лівому верхньому кутку та вибираємо «Видалити Елементи» 
+    після чого зазначуємо квадратики біля елементів які хочемо видалити
+    та натискаємо кнопку «Видалити вибрані елементи». 
+   
+б)  Шоб видалити всі елементи зі списку клацаємо символ «⨂» на панелі меню 
     в лівому верхньому кутку та вибираємо «Видалити Все ❗❗❗» після чого 
     підтверджуємо що хочемо видалити всі елементи клацаючи кнопку «ok».
-
-
-
-
-
-
+    
+    
                                                                                         автор: Антоній Гуйван
             """
 hu_pl = """
-
-
                                                     UŻYTKOWANIE
-
-
+                                                    
+                                                    
 1. Wprowadzenie i dodanie elementu do listy
-    a) Wprowadzenie kwoty
-    b) Wprowadzenie daty
-    c) Wprowadzenie opisu
-    d) Dodanie do listy
-
+   a) Wprowadzenie kwoty
+   b) Wprowadzenie daty
+   c) Wprowadzenie opisu
+   d) Dodanie do listy
 2. Zmiana układu wyświetlania
 3. Zmiana języka
 4. Usuwanie elementów
- 
-
+   a) Usuwanie wybranych elementów
+   b) Usuwanie wszystkich elementów
 
 1.
-    
+
     Warto zaznaczyć że na każdym etapie wpisywania możemy skorzystać
     z przycisku 'Anuluj' w celu anulowania całego procesu wpisywania
     nowego elementu, co usunie podane wcześniej dane.
-    
+
 a)
-    
+
     Aby wprowadzić kwotę należy wpisać w polu tekstowym 
     (zielony odcień tła) liczbę, można oddzielić kropką lub przecinkiem. 
     Przykłady: «48»; «24,23»; «15.22»; «-2»; «-3.8».
     Liczby bez znaku są uwzględniane jako przychód,
     a liczby z minusem przed, są uwzględniane jako wydatek.
     Po czym należy kliknąć przycisk z symbolem '↲' lub nacisnąć enter. 
-    
+
     Uwaga! Przycisk nie zadziała jeśli wartość jest niepoprawna,
     popraw wartość i spróbuj ponownie.
-    
+
 b)
-    
+
     Aby wprowadzić datę należy wpisać w polu tekstowym (zielony odcień tła) 
     datę oddzieloną myślnikami, format następujący: RRRR-MM-DD 
     Przykłady: «2000-02-29»; «1998-12-07»; «2022-01-13».
     Po czym należy kliknąć przycisk z symbolem '↲' lub nacisnąć enter. 
-    
+
     Uwaga! Przycisk nie zadziała jeśli wartość jest niepoprawna,
     popraw wartośc i spróbuj ponownie.
-    
+
 c)
-    
+
     Aby wprowadzić opis należy wpisać w polu tekstowym (zielony odcień tła) 
     dowolny opis. Przykłady: «Zakupy»; «Wypłata»; «Wygrana ze zdrapki».
     Po czym należy kliknąć przycisk z symbolem '↲' lub nacisnąć enter. 
-    
+
 d)
-    
+
     Aby dodać element do listy warto najpierw przeczytać czy dane się zgadzają.
-    
-    Uwaga! Funkcja usuwania wybranych elementów jeszcze nie została stworzona,
-    jeśli nie chcemy usuwać wszystkich elementów
-    warto się zastanowić przed dodaniem. 
-    
     Po czym gdy już jesteśmy pewni że chcemy dodać nasz element do listy
     klikamy przycisk 'Potwierdź'.
-    
-2. 
 
+2. 
     W celu zmiany układu wyświetlania listy klikamy symbol '⧫' na panelu menu 
     w lewym górnym rogu, po czym wybieramy 'Według Daty' lub 'Układ Antona'.
     Układ domyślny to 'Według Daty'.
-    
-3.
 
+3.
     Aby zmienić język należy kliknąć symbol '⨁' na panelu menu
     w lewym górnym rogu i wybrać język aplikacji na który chcielibyśmy zmienić.
-    
+
     Uwaga! Język należy zmienić zanim zaczniemy wprowadzać dane,
     wprowadzenie danych i zmiana języka w trakcie, będzie skutkować 
     usunięciem wprowadzonych i nie zapisanych do listy danych.
-    
-4.
 
+4.
+  
+a)  
+    Aby usunąć wybrane elementy z listy klikamy symbol '⨂' na panelu menu
+    w lewym górnym rogu i wybieramy 'Usuń elementy' po czym zaznaczamy 
+    kwadraciki obok elementów które chcemy usunąć i klikamy przycisk 
+    'Usuń wybrane elementy'.
+    
+b)
     Aby usunąć wszystkie elementy z listy klikamy symbol '⨂' na panelu menu
     w lewym górnym rogu i wybieramy 'Usuń wszystko ❗❗❗' po czym 
     zatwierdzamy że chcemy usunąć wszystkie elementy klikając przycisk 'ok'.
-
-
-
-
-
-
+    
+    
                                                                                         autor: Antonij Gujwan
             """
 hu_en = """
-
-
                                                          USAGE
-
-
+                                                         
+                                                         
 1. Inserting and adding element to a list
-    a) Inserting amount
-    b) Inserting date
-    c) Inserting description
-    d) Adding to a list
-
+   a) Inserting amount
+   b) Inserting date
+   c) Inserting description
+   d) Adding to a list
 2. Changing list's view
 3. Changing language
 4. Deleting elements
- 
-
+   a) Deleting chosen elements
+   b) Deleting all elements
 
 1.
-    
+
     It's important to remember that on every step of inserting we can click
     'Cancel' to cancel whole operation of creating new element,
     that will erase data typed earlier and allow us to start again.
-    
+
 a)
-    
+
     To insert amount, type in text field (green background) a number,
     you can separate with a dot or a comma. 
     Examples: «48»; «24,23»; «15.22»; «-2»; «-3.8».
     Numbers without a sign are taken as income,
     numbers with minus, are taken as expenses.
     Then click on a button with the symbol of '↲' or press enter. 
-    
+
     Warning! The button won't work if a value is incorrect,
     correct the value and try again.
-    
+
 b)
-    
+
     To insert date, type in text field (green background) a date 
     separated with dashes, following format: YYYY-MM-DD 
     Examples: «2000-02-29»; «1998-12-07»; «2022-01-13».
     Then click on a button with the symbol of '↲' or press enter. 
-    
+
     Warning! The button won't work if a value is incorrect,
     correct the value and try again.
-    
+
 c)
-    
+
     To insert description, type in text field (green background) 
     your description. Examples: «Shopping»; «Pension»; «A gift from friend».
     Then click on a button with the symbol of '↲' or press enter. 
-    
+
 d)
-    
+
     In order to add an element to the list it's worth reading your data first.
-    
-    Warning! A function of deletion of chosen 
-    elements hasn't been implemented yet,
-    if you don't want to delete a whole list
-    it's worth rethinking adding new element. 
-    
     After you're sure that you want to add your element to the list,
     click button 'Apply'.
     
 2. 
-
     In order to change view of a list click a symbol of '⧫' on menu panel
     on upper - left corner, and choose 'Dated' or 'Anton's Arrange'.
     The default is 'Dated'.
-    
-3.
 
+3.
     In order to change language click a symbol of '⨁' on menu panel on
     upper - left corner and choose program's language that you want.
-    
+
     Warning! Change the language before inserting data,
     language change during inserting data, will lead to 
     an erasure of data typed earlier and not added to the list.
-    
+
 4.
 
+a)  
+    In order to delete chosen elements from a list  
+    click a symbol of «⨂» on menu panel on upper - left corner
+    and choose 'Delete Items' then check checkboxes of elements
+    you want to delete and click 'Delete selected items'.
+
+b)
     In order to delete all of the elements from a list  
     click a symbol of «⨂» on menu panel on upper - left corner
     and choose 'Delete All ❗❗❗' then confirm it by clicking 'ok'.
-
-
-
-
-
-
+    
+    
                                                                                     the author: Antonij Gujwan
             """
